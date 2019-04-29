4c1df1ab9a81d26f5c2009bb0405d1c25e709984
nmparfait <mbamu.developer@gmail.com>
2019-04-28 21:54:00 -0400

    new jascript started

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var  francais = document.querySelector(".version-francais");
+console.log("fui clicado");
+
+
+//francais.addEventListener("click", function showFrancais() {
+  //  console.log("Quero aparecer na página");
+//});
+
